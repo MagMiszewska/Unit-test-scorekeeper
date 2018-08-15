c82a49fadae47f52a11923dbfe202698c6dec185
Magdalena Miszewska <mag.miszewska@gmail.com>
2018-08-15 17:05:40 +0200

    Add AddPlayer component, create tests for this component

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 import PlayersList from './components/PlayersList/PlayersList';
+import AddPlayer from './components/AddPlayer/AddPlayer';
 
 it('renders without crashing', () => {
   shallow(<App />);
@@ -24,4 +25,14 @@ it('should player score', () => {
   onScoreUpdate(0, 5);
   const playersAfterUpdate = appComponent.state('players');
   expect(playersAfterUpdate[0].score).toEqual(10);
+});
+
+it('should add new player', () => {
+  const appComponent = shallow(<App />);
+  const onPlayerAdd = appComponent.find(AddPlayer).prop('onPlayerAdd');
+  onPlayerAdd('Ania');
+  const players = appComponent.state('players');
+  expect(players.length).toEqual(1);
+  expect(players[0].name).toEqual('Ania');
+  expect(players[0].score).toEqual(0);
 });
